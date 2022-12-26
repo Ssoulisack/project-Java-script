330e0e25b636a85cab92cc0e10f98eabb7b4d24a
Soulisack Douangvilay <Ssoulisack>
2022-12-26 15:52:19 +0700

    sack

--- js/jsscript.js
+++ js/jsscript.js
@@ -13,6 +13,7 @@ document.querySelector('#search-btn').onclick = () =>{
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
+
 let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
